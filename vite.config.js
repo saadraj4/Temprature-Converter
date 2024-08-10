4886f5f2db0bfa0c8358b56566af2b354287c49c
Saad Raja <sr201040@gmail.com>
2024-08-10 22:21:35 +0500

    Minor Changes

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Allows access from other devices on the same network
-    port: 5173,      // Optional: You can specify the port, otherwise it uses the default 5173
-  },
+ 
 })
